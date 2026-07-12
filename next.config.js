cfd37c3b30fa0108ce0bfbe7b31b09628c9477fd
Nachiket Kale <nachiket.kale24@spit.ac.in>
2026-07-12 22:41:27 +0530

    chore: configure static export and add deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
   compiler: {
